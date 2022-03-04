20d42dd467ed856dbda06cad97d7ee7f2a641754
Imya Familya <kodtya.tishkov051@gmail.com>
2022-03-04 13:11:18 +0700

    Всё работает!

--- Cinema/Forms/SearchForm.Designer.cs
+++ Cinema/Forms/SearchForm.Designer.cs
@@ -33,7 +33,6 @@ namespace Cinema
             this.defaultButton = new System.Windows.Forms.Button();
             this.checkBox2 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.filmsComboBox = new System.Windows.Forms.ComboBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
@@ -44,20 +43,9 @@ namespace Cinema
             this.label3 = new System.Windows.Forms.Label();
             this.checkBox3 = new System.Windows.Forms.CheckBox();
             this.dateFilterPanel = new System.Windows.Forms.Panel();
-            this.filmFilterPanel = new System.Windows.Forms.Panel();
-            this.checkBox4 = new System.Windows.Forms.CheckBox();
-            this.occupancyFilterPanel = new System.Windows.Forms.Panel();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.button1 = new System.Windows.Forms.Button();
             this.roomFilterPanel.SuspendLayout();
             this.dateFilterPanel.SuspendLayout();
-            this.filmFilterPanel.SuspendLayout();
-            this.occupancyFilterPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // searchBox
@@ -67,14 +55,13 @@ namespace Cinema
             this.searchBox.Name = "searchBox";
             this.searchBox.Size = new System.Drawing.Size(360, 27);
             this.searchBox.TabIndex = 0;
-            this.searchBox.TextChanged += new System.EventHandler(this.searchBox_TextChanged);
+            this.searchBox.TextChanged += new System.EventHandler(this.SearchBox_TextChanged);
             // 
             // defaultButton
             // 
             this.defaultButton.AutoSize = true;
             this.defaultButton.BackColor = System.Drawing.SystemColors.Control;
-            this.defaultButton.Enabled = false;
-            this.defaultButton.Location = new System.Drawing.Point(16, 397);
+            this.defaultButton.Location = new System.Drawing.Point(16, 241);
             this.defaultButton.Name = "defaultButton";
             this.defaultButton.Size = new System.Drawing.Size(160, 32);
             this.defaultButton.TabIndex = 1;
@@ -106,27 +93,19 @@ namespace Cinema
             this.label1.TabIndex = 6;
             this.label1.Text = "Поиск:";
             // 
-            // filmsComboBox
-            // 
-            this.filmsComboBox.FormattingEnabled = true;
-            this.filmsComboBox.Location = new System.Drawing.Point(4, 32);
-            this.filmsComboBox.Name = "filmsComboBox";
-            this.filmsComboBox.Size = new System.Drawing.Size(352, 27);
-            this.filmsComboBox.TabIndex = 8;
-            this.filmsComboBox.Visible = false;
-            // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
             this.radioButton1.ForeColor = System.Drawing.Color.White;
             this.radioButton1.Location = new System.Drawing.Point(4, 32);
             this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(120, 23);
+            this.radioButton1.Size = new System.Drawing.Size(36, 23);
             this.radioButton1.TabIndex = 0;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "Кинозал №1";
+            this.radioButton1.Tag = "";
+            this.radioButton1.Text = "1";
             this.radioButton1.UseVisualStyleBackColor = true;
             this.radioButton1.Visible = false;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // radioButton2
             // 
@@ -134,12 +113,14 @@ namespace Cinema
             this.radioButton2.ForeColor = System.Drawing.Color.White;
             this.radioButton2.Location = new System.Drawing.Point(4, 62);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(120, 23);
+            this.radioButton2.Size = new System.Drawing.Size(36, 23);
             this.radioButton2.TabIndex = 1;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "Кинозал №2";
+            this.radioButton2.Tag = "";
+            this.radioButton2.Text = "2";
             this.radioButton2.UseVisualStyleBackColor = true;
             this.radioButton2.Visible = false;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // radioButton3
             // 
@@ -147,12 +128,14 @@ namespace Cinema
             this.radioButton3.ForeColor = System.Drawing.Color.White;
             this.radioButton3.Location = new System.Drawing.Point(237, 32);
             this.radioButton3.Name = "radioButton3";
-            this.radioButton3.Size = new System.Drawing.Size(120, 23);
+            this.radioButton3.Size = new System.Drawing.Size(36, 23);
             this.radioButton3.TabIndex = 2;
             this.radioButton3.TabStop = true;
-            this.radioButton3.Text = "Кинозал №3";
+            this.radioButton3.Tag = "";
+            this.radioButton3.Text = "3";
             this.radioButton3.UseVisualStyleBackColor = true;
             this.radioButton3.Visible = false;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // radioButton4
             // 
@@ -160,12 +143,14 @@ namespace Cinema
             this.radioButton4.ForeColor = System.Drawing.Color.White;
             this.radioButton4.Location = new System.Drawing.Point(236, 62);
             this.radioButton4.Name = "radioButton4";
-            this.radioButton4.Size = new System.Drawing.Size(120, 23);
+            this.radioButton4.Size = new System.Drawing.Size(36, 23);
             this.radioButton4.TabIndex = 3;
             this.radioButton4.TabStop = true;
-            this.radioButton4.Text = "Кинозал №4";
+            this.radioButton4.Tag = "";
+            this.radioButton4.Text = "4";
             this.radioButton4.UseVisualStyleBackColor = true;
             this.radioButton4.Visible = false;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.RadioButton_CheckedChanged);
             // 
             // roomFilterPanel
             // 
@@ -176,7 +161,7 @@ namespace Cinema
             this.roomFilterPanel.Controls.Add(this.radioButton2);
             this.roomFilterPanel.Controls.Add(this.radioButton3);
             this.roomFilterPanel.Controls.Add(this.radioButton4);
-            this.roomFilterPanel.Location = new System.Drawing.Point(12, 132);
+            this.roomFilterPanel.Location = new System.Drawing.Point(12, 64);
             this.roomFilterPanel.Name = "roomFilterPanel";
             this.roomFilterPanel.Size = new System.Drawing.Size(360, 88);
             this.roomFilterPanel.TabIndex = 2;
@@ -232,99 +217,18 @@ namespace Cinema
             this.dateFilterPanel.Controls.Add(this.dateTimePicker1);
             this.dateFilterPanel.Controls.Add(this.label3);
             this.dateFilterPanel.Controls.Add(this.dateTimePicker2);
-            this.dateFilterPanel.Location = new System.Drawing.Point(12, 223);
+            this.dateFilterPanel.Location = new System.Drawing.Point(12, 158);
             this.dateFilterPanel.Name = "dateFilterPanel";
             this.dateFilterPanel.Size = new System.Drawing.Size(360, 62);
             this.dateFilterPanel.TabIndex = 14;
-            // 
-            // filmFilterPanel
-            // 
-            this.filmFilterPanel.AutoSize = true;
-            this.filmFilterPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(79)))), ((int)(((byte)(79)))));
-            this.filmFilterPanel.Controls.Add(this.checkBox4);
-            this.filmFilterPanel.Controls.Add(this.filmsComboBox);
-            this.filmFilterPanel.Location = new System.Drawing.Point(12, 288);
-            this.filmFilterPanel.Name = "filmFilterPanel";
-            this.filmFilterPanel.Size = new System.Drawing.Size(360, 62);
-            this.filmFilterPanel.TabIndex = 15;
-            // 
-            // checkBox4
-            // 
-            this.checkBox4.AutoSize = true;
-            this.checkBox4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.checkBox4.ForeColor = System.Drawing.Color.White;
-            this.checkBox4.Location = new System.Drawing.Point(4, 3);
-            this.checkBox4.Name = "checkBox4";
-            this.checkBox4.Size = new System.Drawing.Size(118, 23);
-            this.checkBox4.TabIndex = 15;
-            this.checkBox4.Tag = "Film";
-            this.checkBox4.Text = "По фильму";
-            this.checkBox4.UseVisualStyleBackColor = true;
-            this.checkBox4.CheckedChanged += new System.EventHandler(this.TurnOnFilter);
-            // 
-            // occupancyFilterPanel
-            // 
-            this.occupancyFilterPanel.AutoSize = true;
-            this.occupancyFilterPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(79)))), ((int)(((byte)(79)))));
-            this.occupancyFilterPanel.Controls.Add(this.checkBox1);
-            this.occupancyFilterPanel.Controls.Add(this.label2);
-            this.occupancyFilterPanel.Controls.Add(this.numericUpDown2);
-            this.occupancyFilterPanel.Controls.Add(this.numericUpDown1);
-            this.occupancyFilterPanel.Location = new System.Drawing.Point(12, 64);
-            this.occupancyFilterPanel.Name = "occupancyFilterPanel";
-            this.occupancyFilterPanel.Size = new System.Drawing.Size(360, 65);
-            this.occupancyFilterPanel.TabIndex = 16;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
-            this.checkBox1.ForeColor = System.Drawing.Color.White;
-            this.checkBox1.Location = new System.Drawing.Point(4, 3);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(199, 23);
-            this.checkBox1.TabIndex = 15;
-            this.checkBox1.Tag = "Occupancy";
-            this.checkBox1.Text = "По вместительности";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.TurnOnFilter);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(170, 37);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(15, 19);
-            this.label2.TabIndex = 18;
-            this.label2.Text = "-";
-            this.label2.Visible = false;
-            // 
-            // numericUpDown2
-            // 
-            this.numericUpDown2.Location = new System.Drawing.Point(194, 35);
-            this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(162, 27);
-            this.numericUpDown2.TabIndex = 17;
-            this.numericUpDown2.Visible = false;
-            // 
-            // numericUpDown1
-            // 
-            this.numericUpDown1.Location = new System.Drawing.Point(4, 35);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(160, 27);
-            this.numericUpDown1.TabIndex = 16;
-            this.numericUpDown1.Visible = false;
-            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumericUpDown1_ValueChanged);
             // 
             // button1
             // 
             this.button1.AutoSize = true;
             this.button1.BackColor = System.Drawing.SystemColors.Control;
-            this.button1.Enabled = false;
-            this.button1.Location = new System.Drawing.Point(182, 398);
+            this.button1.Location = new System.Drawing.Point(206, 242);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(187, 31);
+            this.button1.Size = new System.Drawing.Size(166, 31);
             this.button1.TabIndex = 17;
             this.button1.Text = "Выполнить";
             this.button1.UseVisualStyleBackColor = false;
@@ -335,14 +239,12 @@ namespace Cinema
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(63)))), ((int)(((byte)(59)))));
-            this.ClientSize = new System.Drawing.Size(384, 441);
+            this.ClientSize = new System.Drawing.Size(384, 284);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.searchBox);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.occupancyFilterPanel);
             this.Controls.Add(this.roomFilterPanel);
             this.Controls.Add(this.dateFilterPanel);
-            this.Controls.Add(this.filmFilterPanel);
             this.Controls.Add(this.defaultButton);
             this.Font = new System.Drawing.Font("Tahoma", 12F);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
@@ -354,12 +256,6 @@ namespace Cinema
             this.roomFilterPanel.PerformLayout();
             this.dateFilterPanel.ResumeLayout(false);
             this.dateFilterPanel.PerformLayout();
-            this.filmFilterPanel.ResumeLayout(false);
-            this.filmFilterPanel.PerformLayout();
-            this.occupancyFilterPanel.ResumeLayout(false);
-            this.occupancyFilterPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -371,7 +267,6 @@ namespace Cinema
         private System.Windows.Forms.Button defaultButton;
         private System.Windows.Forms.CheckBox checkBox2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox filmsComboBox;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
@@ -382,13 +277,6 @@ namespace Cinema
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckBox checkBox3;
         private System.Windows.Forms.Panel dateFilterPanel;
-        private System.Windows.Forms.Panel filmFilterPanel;
-        private System.Windows.Forms.CheckBox checkBox4;
-        private System.Windows.Forms.Panel occupancyFilterPanel;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.NumericUpDown numericUpDown2;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button1;
     }
 }
--- Cinema/Forms/SearchForm.cs
+++ Cinema/Forms/SearchForm.cs
@@ -26,29 +26,16 @@ namespace Cinema
         {
             this.Dispose();
         }
-        string connectionString = Properties.Settings.Default.BaseConnectionString;
-        bool filterOccu = false;
         bool filterRoom = false;
         bool filterDate = false;
-        bool filterFilm = false;
-
-        private void NumericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-            numericUpDown2.Minimum = numericUpDown1.Value;
-        }
+        bool bothFilters = false;
+        int room;
 
         private void TurnOnFilter(object sender, EventArgs e)
         {
             Control c = sender as Control;
             switch (c.Tag)
             {
-                case "Occupancy":
-                    filterOccu = !filterOccu;
-                    numericUpDown1.Visible = !numericUpDown1.Visible;
-                    numericUpDown2.Visible = !numericUpDown2.Visible;
-                    label2.Visible = !label2.Visible;
-                    break;
-
                 case "Room":
                     filterRoom = !filterRoom;
                     radioButton1.Visible = !radioButton1.Visible;
@@ -64,59 +51,90 @@ namespace Cinema
                     label3.Visible = !label3.Visible;
                     break;
 
-                case "Film":
-                    filterFilm = !filterFilm;
-                    filmsComboBox.Visible = !filmsComboBox.Visible;
-                    break;
-
             }
-            if ((filterDate || filterFilm || filterOccu || filterRoom))
+            bothFilters = false;
+            if(filterDate && filterRoom)
             {
-                button1.Enabled = !button1.Enabled;
-                defaultButton.Enabled = !defaultButton.Enabled;
-            }
-            else
-            {
-                button1.Enabled = !button1.Enabled;
-                defaultButton.Enabled = !defaultButton.Enabled;
+                filterRoom = false;
+                filterDate = false;
+                bothFilters = true;
             }
         }
 
         private void DefaultButton_Click(object sender, EventArgs e)
         {
-
             formHome.LoadTable();
         }
         private void FilterButton_Click(object sender, EventArgs e)
         {
-            
+            //SELECT * FROM [Session] WHERE ([Occupancy] BETWEEN '20' AND '60') AND [RoomID] = '1'
+            // Только один параметр | SELECT * FROM [Session] WHERE @text
+            // Два и более | SELECT * FROM [Session] WHERE (@text) AND () AND ()
 
-
-
-            if (filterOccu)
+            //SELECT * FROM [Session] WHERE ([Date] BETWEEN '2022-01-01' AND '2022-03-1') AND [RoomID] = '1'
+            string filterText = "";
+            if (bothFilters)
             {
-                DataSet data = new DataSet();
-                SqlDataAdapter adapter = new SqlDataAdapter("session_occupancy_filter", connectionString);
-                SqlParameter parameterV1 = new SqlParameter
-                {
-                    ParameterName = "@VALUE1",
-                    Value = numericUpDown1.Value
-                };
-                SqlParameter parameterV2 = new SqlParameter
-                {
-                    ParameterName = "@VALUE2",
-                    Value = numericUpDown2.Value
-                };
-                adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
-                adapter.SelectCommand.Parameters.Add(parameterV1);
-                adapter.SelectCommand.Parameters.Add(parameterV2);
-                adapter.Fill(data);
-                formHome.sessionTable.DataSource = data.Tables[0];
-
+                filterText = $"SELECT * FROM [Session] WHERE ([Date] " +
+                             $"BETWEEN '{dateTimePicker1.Value.ToShortDateString()}' " +
+                             $"AND '{dateTimePicker2.Value.ToShortDateString()}' " +
+                             $"AND [RoomID] = '{room}'";
             }
+            if (filterRoom)
+            {
+                filterText += $"SELECT * FROM [Session] WHERE " +
+                              $"[RoomID] = '{room}'";
+            }
+            if (filterDate)
+            {
+                filterText += $"SELECT * FROM [Session] WHERE" +
+                              $"[Date] BETWEEN '{dateTimePicker1.Value.ToShortDateString()}' " +
+                              $"AND '{dateTimePicker2.Value.ToShortDateString()}'";
+            }
+
+
+            DataSet set = new DataSet();
+            if (!String.IsNullOrEmpty(filterText))
+            {
+                if (room != 0)
+                {
+                    set = Filtration(filterText);
+                    formHome.sessionTable.DataSource = set.Tables[0];
+                }
+            }
+
+            //if (filterOccu)
+            //{
+            //    DataSet data = new DataSet();
+            //    SqlDataAdapter adapter = new SqlDataAdapter("session_occupancy_filter", connectionString);
+            //    SqlParameter parameterV1 = new SqlParameter
+            //    {
+            //        ParameterName = "@VALUE1",
+            //        Value = numericUpDown1.Value
+            //    };
+            //    SqlParameter parameterV2 = new SqlParameter
+            //    {
+            //        ParameterName = "@VALUE2",
+            //        Value = numericUpDown2.Value
+            //    };
+            //    adapter.SelectCommand.CommandType = CommandType.StoredProcedure;
+            //    adapter.SelectCommand.Parameters.Add(parameterV1);
+            //    adapter.SelectCommand.Parameters.Add(parameterV2);
+            //    adapter.Fill(data);
+            //    formHome.sessionTable.DataSource = data.Tables[0];
+
+            //}
         }
 
-        private void searchBox_TextChanged(object sender, EventArgs e)
+        private static DataSet Filtration(string filterText)
+        {
+            DataSet set = new DataSet();
+            SqlDataAdapter adapter = new SqlDataAdapter(filterText,Properties.Settings.Default.BaseConnectionString);
+            adapter.Fill(set);
+            return set;
+        }
+
+        private void SearchBox_TextChanged(object sender, EventArgs e)
         {
             formHome.sessionTable.ClearSelection();
             int rows = formHome.sessionTable.RowCount;
@@ -124,7 +142,6 @@ namespace Cinema
             string searchText = searchBox.Text;
             formHome.sessionTable.MultiSelect = true;
             for (int i = 0; i < rows; i++)
-            {
                 for (int j = 0; j < columns; j++)
                 {
                     if (formHome.sessionTable.Rows[i].Cells[j].Value != null)
@@ -134,7 +151,11 @@ namespace Cinema
                             return;
                         }
                 }
-            }
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            room = Convert.ToInt32((sender as RadioButton).Text);
         }
     }
 }
